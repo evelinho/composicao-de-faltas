ed0448459c1468e91d6e8ec71bf31cd6fcb48fcf
evelinho <00001117347989sp@al.educacao.sp.gov.br>
2024-09-24 05:14:56 -0300

    Primeiro commit

--- storage/js/index.js
+++ storage/js/index.js
@@ -10,65 +10,6 @@ const scroll = function() {
         }
     });
 }
-
-const slideMove = function() {
-    const home = document.getElementById('home');
-    const btns = document.querySelectorAll('#home .btn-slider .btn');
-    let currentIndex = 0;
-    let slideInterval;
-
-    const changeSlide = (index) => {
-        btns.forEach(b => b.classList.remove('active'));
-        btns[index].classList.add('active');
-        home.style.backgroundImage = `url('./storage/img/background-desktop-${index}.jpg')`;
-    };
-    const startAutoSlide = () => {
-        slideInterval = setInterval(() => {
-            currentIndex = (currentIndex + 1) % btns.length;
-            changeSlide(currentIndex);
-        }, 4000);
-    };
-    btns.forEach((btn, index) => {
-        btn.addEventListener('click', function() {
-            clearInterval(slideInterval);
-            currentIndex = index;
-            changeSlide(currentIndex);
-        });
-    });
-
-    startAutoSlide();
-};
-
-const catalogoSlider = function() {
-    const slider = document.querySelector('#catalogo .list-slide-movies');
-    const slides = document.querySelectorAll('.slider');
-    const prevSlide = document.getElementById('prev-slide');
-    const nextSlide = document.getElementById('next-slide');
-    let slideCurrent = 0;
-    let width = slides[0].offsetWidth;
-
-    const runSliderCatalogo = function(next) {
-        let slideNumber = 2;
-        let gap = 64;
-
-        if (next && slideCurrent < ((slides.length / slideNumber))) {
-            slideCurrent += 2;
-        }
-        if (!next && slideCurrent >= 2) {
-            slideCurrent = slideCurrent - 2;
-            console.log(`${slideCurrent} = ${slideCurrent} - ${2}`);
-        }
-        console.log(slideCurrent);
-        slider.style.cssText = `transform: translateX(-${((width + gap)*slideCurrent)}px);`;
-    }
-
-    prevSlide.addEventListener('click', function() {
-        runSliderCatalogo(false);
-    })
-    nextSlide.addEventListener('click', function() {
-        runSliderCatalogo(true);
-    })
-}
 const faq = function() {
     
     document.querySelectorAll('.faq-question').forEach(question => {
@@ -97,8 +38,5 @@ const faq = function() {
 }
 
 
-/*Executando*/
 scroll();
-slideMove();
-catalogoSlider();
 faq();
